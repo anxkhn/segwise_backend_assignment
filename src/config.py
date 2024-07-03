fdb358e7c8865f6e08d5341263c7066fe5fc651a
= <anaskhaaan.28@gmail.com>
2024-07-03 22:07:22 +0530

    :sparkles: add basic api with swagger ui (2/2)

---
+++ src/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "supersecretkey"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL") or "sqlite:///app.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    RATELIMIT_DEFAULT = "5 per minute"
